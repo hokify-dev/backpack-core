410fbacf284e0d2966a074ea9769378d3ac29ce0
Simon <s.tretter@gmail.com>
2018-09-27 17:30:11 +0200

    webpack 4 has it's own json loader now, do not intefere

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -92,10 +92,6 @@ module.exports = options => {
       rules: [
         // This is the development configuration.
         // It is focused on developer experience and fast rebuilds.
-        {
-          test: /\.json$/,
-          loader: require.resolve('json-loader'),
-        },
         // Process JS with Babel (transpiles ES6 code into ES5 code).
         {
           test: /\.(js|jsx)$/,
